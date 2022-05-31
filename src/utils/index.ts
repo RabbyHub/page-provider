12be1b154858c62708731b898afaac88eb5672a1
Hodor <lost1q84@gmail.com>
2022-05-31 10:58:16 +0800

    feat: init (#1)

---
+++ src/utils/index.ts
@@ -0,0 +1,39 @@
+import { CHAINS } from "@debank/common";
+import { keyBy } from "lodash";
+import { browser } from "webextension-polyfill-ts";
+import { ledgerUSBVendorId } from "@ledgerhq/devices";
+
+import BroadcastChannelMessage from "./message/broadcastChannelMessage";
+import PortMessage from "./message/portMessage";
+
+const Message = {
+  BroadcastChannelMessage,
+  PortMessage,
+};
+
+declare global {
+  const langLocales: Record<string, Record<"message", string>>;
+}
+
+const t = (name) => browser.i18n.getMessage(name);
+
+const format = (str, ...args) => {
+  return args.reduce((m, n) => m.replace("_s_", n), str);
+};
+
+export { Message, t, format };
+
+const chainsDict = keyBy(CHAINS, "serverId");
+export const getChain = (chainId?: string) => {
+  if (!chainId) {
+    return null;
+  }
+  return chainsDict[chainId];
+};
+
+export const hasConnectedLedgerDevice = async () => {
+  // const devices = await navigator.hid.getDevices();
+  // return (
+  //   devices.filter((device) => device.vendorId === ledgerUSBVendorId).length > 0
+  // );
+};
