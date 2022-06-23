72333ce2a80572f66f5e402b784f0eeb1f21f53e
Hodor <lost1q84@gmail.com>
2022-06-23 14:50:58 +0800

    fix: metamsk conflict

---
+++ src/pageProvider/interceptors/switchWallet.ts
@@ -0,0 +1,32 @@
+import IconMetamask from "../../assets/metamask.svg";
+import IconRabby from "../../assets/rabby.svg";
+import notice from "../notice";
+
+let instance: ReturnType<typeof notice> | null;
+
+export const switchWalletNotice = (type: "rabby" | "metamask") => {
+  if (instance) {
+    instance.hide();
+    instance = null;
+  }
+  instance = notice({
+    closeable: true,
+    timeout: 0,
+    className: "rabby-notice-default-wallet",
+    content: `<div style="display: flex; align-items: center; gap: 12px;">
+      <img style="width: 28px;" src="${
+        type === "rabby" ? IconRabby : IconMetamask
+      }"/>
+      <div>
+        <div><span style="font-weight: bold; text-transform: capitalize;">${type}</span> is your default wallet now. </div>
+        <div>
+        Please <a
+          href="javascript:window.location.reload();"
+          style="color: #8697FF; text-decoration: underline;">refresh the web page</a> 
+        and retry
+        </div>
+      </div>
+    </div>
+    `,
+  });
+};
