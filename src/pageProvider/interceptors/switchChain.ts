0b5da10db61d76a7d4455d94dcb33fb29b8625b1
Hodor <lost1q84@gmail.com>
2024-04-26 17:20:44 +0800

    feat: change switch chain style

--- src/pageProvider/interceptors/switchChain.ts
+++ src/pageProvider/interceptors/switchChain.ts
@@ -10,6 +10,7 @@ interface Chain {
   [key: string]: any;
   id: number;
   name: string;
+  logo?: string;
   isTestnet: boolean;
 }
 
@@ -25,35 +26,13 @@ export const switchChainNotice = (
     instance.hide();
     instance = null;
   }
-  const isSwitchToMainnet =
-    chain?.prev && chain?.prev?.isTestnet && !chain?.isTestnet;
-  const isSwitchToTestnet =
-    chain?.prev && !chain?.prev?.isTestnet && chain?.isTestnet;
 
-  const rawContent = `<img style="width: 20px; margin-right: 8px;" src="${IconRabby}"/>Switched to <span class="rabby-strong" style="margin: 0 8px;">${chain?.name}</span> for the current Dapp`;
+  const rawContent = `<img style="width: 20px; height: 20px; margin-right: 8px;" src="${
+    chain?.logo || IconRabby
+  }"/>Network switched to <span class="rabby-strong" style="margin: 0 8px;">${
+    chain?.name
+  }</span>`;
   let content = rawContent;
-  if (isSwitchToMainnet || isSwitchToTestnet) {
-    content = `
-    <div>
-      <div style="display: flex; align-items: center; justify-content: center; color: #13141A;">
-        ${rawContent}
-      </div>
-      <div style="display: flex;align-items:center;gap:6px;margin-top:8px;padding-top:8px;border-top:1px solid #d3d8e0;border-top-width:0.5px; color: #13141A;">
-        <img style="width: 14px;" src="${IconWarning}"/>
-        ${
-          isSwitchToMainnet
-            ? `Testnet <img style="width: 14px;" src="${IconArrow}"/> Mainnet`
-            : ""
-        }
-        ${
-          isSwitchToTestnet
-            ? `Mainnet <img style="width: 14px;" src="${IconArrow}"/> Testnet`
-            : ""
-        }
-      </div>
-    </div>
-    `;
-  }
 
   instance = notice({
     timeout: 3000,
