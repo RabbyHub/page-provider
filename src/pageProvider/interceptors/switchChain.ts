1119f443665306500ec6220a23682e6f73c6f66f
Hodor <lost1q84@gmail.com>
2026-03-06 15:29:20 +0800

    chore

--- src/pageProvider/interceptors/switchChain.ts
+++ src/pageProvider/interceptors/switchChain.ts
@@ -1,6 +1,4 @@
 import IconRabby from "../../assets/rabby.svg";
-import IconWarning from "../../assets/warning.svg";
-import IconArrow from "../../assets/arrow.svg";
 import notice from "../notice";
 import { escapeHTML } from "../utils";
 import { isInSameOriginIframe } from "../../utils/iframe";
@@ -30,7 +28,7 @@ export const switchChainNotice = (
 
   const safeChainName = escapeHTML(chain?.name);
 
-  const rawContent = `<img style="width: 20px; height: 20px; margin-right: 8px; margin-bottom:0px;" src="${IconRabby}"/> <div style="color: #192945; padding-right: 2px;">Network switched to <span class="rabby-strong" style="margin: 0;">${safeChainName}</span><div>`;
+  const rawContent = `<img style="width: 20px; height: 20px; margin-right: 8px; margin-bottom:0px;" src="${IconRabby}"/> <div style="color: #192945; padding-right: 2px;">Network switched to <span class="rabby-strong" style="margin: 0;">${safeChainName}</span></div>`;
   let content = rawContent;
 
   instance = notice({
